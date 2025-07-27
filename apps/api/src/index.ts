cccf26f5e59c2a4dab2e9a13aee4cc407006f405
Juan Velasquez <juseve200@gmail.com>
2025-07-27 02:34:52 -0500

    feat: add basic auth

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { basicAuth } from 'hono/basic-auth';
 import { handleBirthdayNotification } from './cron/birthday-notification';
 import birthdaysRoutes from './routes/birthdays';
 import exchangeRoutes from './routes/exchange';
@@ -6,6 +7,21 @@ import usersRoutes from './routes/users';
 import type { Env } from './types/env';
 
 const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Cholongojopa');
+});
+
+app.use(
+  basicAuth({
+    verifyUser: async (username, password, c) => {
+      // TODO: Verify with hash
+      const pass = await c.env.KV.get(`auth:${username}`);
+
+      return pass === password;
+    },
+  })
+);
 
 app.route('/birthdays', birthdaysRoutes);
 app.route('/users', usersRoutes);
