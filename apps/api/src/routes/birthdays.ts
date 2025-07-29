5667855e7c39163c198b701d19657e4176df29b2
Juan Velasquez <juseve200@gmail.com>
2025-07-29 14:25:15 -0500

    feat: add valibot

--- apps/api/src/routes/birthdays.ts
+++ apps/api/src/routes/birthdays.ts
@@ -1,7 +1,25 @@
 import factoryWithDB from '../factories/app-with-db';
 import { getByBirthday, getNextNBirthdays } from '../users/repository';
+import { vValidator } from '@hono/valibot-validator';
+import {
+  object,
+  optional,
+  pipe,
+  string,
+  transform,
+  integer,
+  minValue,
+  maxValue,
+} from 'valibot';
 
 const app = factoryWithDB.createApp();
+
+const NextBirthdaysSchema = object({
+  n: optional(
+    pipe(string(), transform(Number), integer(), minValue(1), maxValue(10)),
+    '5'
+  ),
+});
 
 app.get('/', async (c) => {
   const db = c.get('db');
@@ -10,16 +28,18 @@ app.get('/', async (c) => {
   return c.json(users);
 });
 
-app.on(['GET', 'POST'], '/next', async (c) => {
-  const n = parseInt(c.req.query('n') || '5');
-  if (isNaN(n) || n <= 0 || n > 10) {
-    return c.json({ error: 'Invalid number parameter, min 1, max 10' }, 400);
+app.on(
+  ['GET', 'POST'],
+  '/next',
+  vValidator('query', NextBirthdaysSchema),
+  async (c) => {
+    const { n } = c.req.valid('query');
+
+    const db = c.get('db');
+    const users = await getNextNBirthdays(db, n);
+
+    return c.json(users);
   }
-
-  const db = c.get('db');
-  const users = await getNextNBirthdays(db, n);
-
-  return c.json(users);
-});
+);
 
 export default app;
